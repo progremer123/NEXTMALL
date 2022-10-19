0c8693411b486683001da7434b385e19d3d0335e
irnag <power700996@gmail.com>
2022-10-20 00:19:36 +0900

    first commit

---
+++ pages/contect.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+export default function contect() {
+  return (
+    <div className={styles.main}>
+      <h1>소개</h1>
+      <p>91914224 정석현</p>
+    </div>
+  )
+}
